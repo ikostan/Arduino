99524a7a20d7655d9abd822e76f8e43c56dc17a6
ikostan <igorkostan@gmail.com>
2018-10-28 15:57:57 -0700

    Update lesson_13/lesson_13.ino
    setUpPins() function

--- lesson_13/lesson_13.ino
+++ lesson_13/lesson_13.ino
@@ -4,15 +4,28 @@ const String lessonName = "LESSON 13: Controlling an RGB LED with Arduino";
 const int baudSpeed = 9600;
 const int sleepTime = 1000;
 
+//Pins
+const int pinBlue = 6;
+const int pinGreen = 10;
+const int pinRed = 11;
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(baudSpeed);
   Serial.println(lessonName);
+  setUpPins();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+//Set-up analog pins
+void setUpPins(){
+
+  pinMode(pinBlue, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinRed, OUTPUT);
 }
