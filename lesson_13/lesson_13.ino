c014aec4870a0ec189054e904314be5dcc8d9ca7
ikostan <igorkostan@gmail.com>
2018-10-28 16:18:05 -0700

    Update lesson_13/lesson_13.ino
    testRgb(),  turnOffAllLed(),
     turnOnAllLed()

--- lesson_13/lesson_13.ino
+++ lesson_13/lesson_13.ino
@@ -3,6 +3,12 @@
 const String lessonName = "LESSON 13: Controlling an RGB LED with Arduino";
 const int baudSpeed = 9600;
 const int sleepTime = 1000;
+const int shortSleepTime = 500;
+
+const int ledBrightness = 100; //MIN is 0, MAX is 255
+const int ledOff = 0; //MIN is 0, MAX is 255
+const int ledFullBrightness = 255; //MIN is 0, MAX is 255
+
 
 //Pins
 const int pinBlue = 6;
@@ -20,7 +26,56 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  testRgb();
 }
+
+//Test function - changing RGB colors
+void testRgb(){
+
+  //Blue
+  Serial.println("Blue is on");
+  analogWrite(pinBlue, ledBrightness);
+  delay(sleepTime);
+  //analogWrite(pinBlue, ledOff);
+  turnOffAllLed();
+
+  //Green
+  Serial.println("Green is on");
+  analogWrite(pinGreen, ledBrightness);
+  delay(sleepTime);
+  //analogWrite(pinGreen, ledOff);
+  turnOffAllLed();
+
+  //Red
+  Serial.println("Red is on");
+  analogWrite(pinRed, ledBrightness);
+  delay(sleepTime);
+  //analogWrite(pinRed, ledOff);
+  turnOffAllLed();
+
+  //RGB
+  Serial.println("RGB is on");
+  turnOnAllLed();
+  delay(sleepTime);
+  turnOffAllLed();
+}
+
+//Turn off all LEDs
+void turnOffAllLed(){
+
+  analogWrite(pinBlue, ledOff);
+  analogWrite(pinGreen, ledOff);
+  analogWrite(pinRed, ledOff);
+}
+
+//Turn off all LEDs
+void turnOnAllLed(){
+
+  analogWrite(pinBlue, ledBrightness);
+  analogWrite(pinGreen, ledBrightness);
+  analogWrite(pinRed, ledBrightness);
+}
+
 
 //Set-up analog pins
 void setUpPins(){
