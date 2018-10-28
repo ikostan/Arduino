4204ab155fb5e84f38a4f4ad97c8542ae06243e2
ikostan <igorkostan@gmail.com>
2018-10-28 15:38:50 -0700

    Update lesson_13/lesson_13.ino
    Start a new lesson - 13

---
+++ lesson_13/lesson_13.ino
@@ -0,0 +1,16 @@
+//Source:  https://www.youtube.com/watch?v=gqAHOoqo3OI&list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-&index=13
+
+const String lessonName = "LESSON 13: Controlling an RGB LED with Arduino";
+const int baudSpeed = 9600;
+const int sleepTime = 1000;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
