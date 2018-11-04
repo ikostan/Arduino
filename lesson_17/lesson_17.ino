0b35f09a78c4863a15c8edb358a19e325176f53b
ikostan <igorkostan@gmail.com>
2018-11-04 13:13:23 -0800

    Lesson 17
    Start lesson 17

---
+++ lesson_17/lesson_17.ino
@@ -0,0 +1,15 @@
+//Source:  https://www.youtube.com/watch?v=eaHRHQ5Ca_U&index=18&list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-
+
+const String lessonName = "LESSON 17: Measure Speed of Sound with Arduino and Ultrasonic Sensor";
+const int baudSpeed = 9600;
+const int waitTime = 25; //Wait time in ms
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
