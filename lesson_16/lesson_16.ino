71dec4e5e148e841f98e539f71373c2fca757c3c
ikostan <igorkostan@gmail.com>
2018-11-04 10:24:44 -0800

    Update lesson_16/lesson_16.ino
    Starting "LESSON 16: Controlling a Servo with Arduino"

---
+++ lesson_16/lesson_16.ino
@@ -0,0 +1,20 @@
+//Source:  https://www.youtube.com/watch?v=fE4m7TyLd8M
+
+const String lessonName = "LESSON 16: Controlling a Servo with Arduino";
+const int baudSpeed = 9600;
+
+
+//Pins
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+}
