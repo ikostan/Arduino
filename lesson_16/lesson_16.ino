54dada21a37f615b47a6fbe9f3c2026cdfe921ce
ikostan <igorkostan@gmail.com>
2018-11-04 11:12:47 -0800

    Update lesson_16/lesson_16.ino
    Convering analog data from potentiometer in to voltage + setting up servo pin

--- lesson_16/lesson_16.ino
+++ lesson_16/lesson_16.ino
@@ -4,19 +4,22 @@
 
 const String lessonName = "LESSON 16: Controlling a Servo with Arduino";
 const int baudSpeed = 9600;
-const int waitTime = 500; //Wait time in ms
+const int waitTime = 250; //Wait time in ms
+const float convertionRate = 204.6; // 1023 devided by 5
 
 //Pins
-const int servoControl = 9; //Servo controlling pin
+const int servoControlPin = 9; //Servo controlling pin
 const int potentiometerAnalogPin = A0; //Potentiometer reading pin
 
 //Logs
 const String turnLeft = "<--";
 const String turnRight = "-->";
-const String voltage = "Voltage: ";
+const String potentiometerValue = "Potentiometer: ";
+const String voltageValue = "Voltage: ";
 
 //Variables
 int currentRate;
+float voltage;
 
 //Setup
 void setup() {
@@ -25,13 +28,15 @@ void setup() {
   Serial.begin(baudSpeed); //Sets the data rate in bits per second (baud) for serial data transmission
   Serial.println(lessonName);
   pinMode(potentiometerAnalogPin, INPUT); // set analog pin A0
+  pinMode(servoControlPin, OUTPUT);
 }
 
 //Main loop
 void loop() {
   // put your main code here, to run repeatedly:
 
-  currentRate = analogRead(potentiometerAnalogPin);
-  Serial.println(voltage + String(currentRate));
+  currentRate = analogRead(potentiometerAnalogPin); //Reading from potentiometer
+  voltage = currentRate / convertionRate; //Convert input from potentiometer in to VOLTS
+  Serial.println(potentiometerValue + String(currentRate) + ". " + voltageValue + String(voltage) + ".");
   delay(waitTime);
 }
