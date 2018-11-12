904dde1fc078f60b1732f89da78f249f1687af26
ikostan <igorkostan@gmail.com>
2018-11-11 16:31:48 -0800

    LESSON 15
    LESSON 15: Arduino Color Sensor and RGB LED

---
+++ lesson_15/lesson_15.ino
@@ -0,0 +1,22 @@
+//Source: https://www.youtube.com/watch?v=Ltp3skGLqFM&index=16&list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-
+
+const int baudSpeed = 9600;
+const int waitTime = 25; //Wait time in ms
+
+const String lessonName = "LESSON 15: Super Cool Arduino Color Sensor Project";
+const int pinRed = 11;
+const int pinGreen = 10;
+const int pinBlue = 6;
+
+const int pinS2 = 7;
+const int pinS3 = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
