f64b711db629bf92d779df41e689f0aac7d611be
ikostan <igorkostan@gmail.com>
2018-12-10 11:45:10 -0800

    Create lesson_16_2.ino
    Version #2
    
    LESSON 16: Controlling a Servo with Arduino

---
+++ lesson_16_2/lesson_16_2.ino
@@ -0,0 +1,14 @@
+//LESSON 16: Controlling a Servo with Arduino
+//Source: https://www.youtube.com/watch?v=fE4m7TyLd8M&index=17&list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-
+
+#define lessonName "LESSON 16: Controlling a Servo with Arduino"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
