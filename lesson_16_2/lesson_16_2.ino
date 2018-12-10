6a22ad5cd73a43850ea97baf29a9a80c786a1177
ikostan <igorkostan@gmail.com>
2018-12-10 12:32:12 -0800

    Update lesson_16_2.ino
    LESSON 16 version #2

--- lesson_16_2/lesson_16_2.ino
+++ lesson_16_2/lesson_16_2.ino
@@ -1,11 +1,18 @@
 //LESSON 16: Controlling a Servo with Arduino
-//Source: https://www.youtube.com/watch?v=fE4m7TyLd8M&index=17&list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-
+//Source: https://www.youtube.com/watch?v=fE4m7TyLd8M&index=17&list=PLGs0VKk2DiYx6CMdOQR_hmJ2NbB4mZQn-servo
+
+#include <Servo.h>
 
 #define lessonName "LESSON 16: Controlling a Servo with Arduino"
+#define servoPin 9
+#define potentiometerPin A0
+#define baudSpeed 9600
 
 void setup() {
   // put your setup code here, to run once:
-
+  
+  Serial.begin(9600);
+  Serial.println(lessonName);
 }
 
 void loop() {
