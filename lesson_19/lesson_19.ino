e3dab7a3e531845336d0e8d343a8541f398db3a3
ikostan <igorkostan@gmail.com>
2018-12-15 19:06:21 -0800

    Create lesson_19.ino
    Start a new lesson (#19)

---
+++ lesson_19/lesson_19.ino
@@ -0,0 +1,15 @@
+// LESSON 19: Using LCD Display with Arduino
+// Source: http://www.toptechboy.com/arduino/lesson-19-arduino-lcd-display/
+// Youtube: http://www.toptechboy.com/arduino/lesson-19-arduino-lcd-display/
+
+#define tutorialName "LESSON 19: Using LCD Display with Arduino"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
